20ebeb7ab6f6b86a6c4c23735f6fa459b299c242
darlingPc <rombk2711@gmail.com>
2023-09-06 21:17:29 +0700

    Initial commit

---
+++ src/main/java/com/example/jobcentrebackend/JobCentreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.jobcentrebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobCentreBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobCentreBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jobcentrebackend/JobCentreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jobcentrebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobCentreBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
